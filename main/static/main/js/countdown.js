70320558b778c3d144ad48d2dff4600c63151c90
valentyn <valentyncherkasov24@gmail.com>
2020-03-22 18:13:03 +0200

    Update requirements.txt

--- main/static/main/js/countdown.js
+++ main/static/main/js/countdown.js
@@ -3,7 +3,20 @@ let time = startingMinutes * 60;
 
 const countdownEl = document.getElementById('countdown');
 
-setInterval(updateCountDown, 1000);
+
+function CounterCaller() {
+	setInterval(updateCountDown, 1000);
+}
+
+function CounterReset() {
+	const minutes = Math.floor(time / 60);
+	
+	let seconds = time % 60;
+
+	seconds = seconds < 10 ? '0' + seconds : seconds
+
+	countdownEl.innerHTML = `${minutes}:${seconds}`;
+}
 
 function updateCountDown() {
 	const minutes = Math.floor(time / 60);
