b5408c180d701b92f475d983dd91e447eb6a0554
Valentyn <valentyncherkasov24@gmail.com>
2020-04-23 18:18:15 +0300

    Fixed problem with model

--- todolist/settings.py
+++ todolist/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'm!om-s-4jzta)e6-2&ve5z+0#3f-f#9nqcy_=h6#=ckgcpb0$('
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'm!om-s-4jzta)e6-2&ve5z+0#3f-f#9nqcy_=h6#=ckgcpb0$('
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
